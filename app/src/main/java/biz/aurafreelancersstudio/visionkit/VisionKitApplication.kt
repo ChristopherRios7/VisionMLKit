843326b76e28024ce94828b5fbf824299469d755
ChristopherRios7 <crios9694@gmail.com>
2018-11-05 11:50:09 -0600

    Update VisionKitApplication.kt

--- app/src/main/java/biz/aurafreelancersstudio/visionkit/VisionKitApplication.kt
+++ app/src/main/java/biz/aurafreelancersstudio/visionkit/VisionKitApplication.kt
@@ -2,7 +2,7 @@ package biz.aurafreelancersstudio.visionkit
 
 import android.app.Application
 
-
+//creates the entire application
 class VisionKitApplication: Application() {
 
     override fun onCreate() {
