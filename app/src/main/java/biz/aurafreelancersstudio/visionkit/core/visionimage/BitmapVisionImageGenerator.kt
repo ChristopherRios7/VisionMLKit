cbbfb1fb941d349b1c8e3cb98978e5414f4170d2
ChristopherRios7 <crios9694@gmail.com>
2018-11-05 11:00:43 -0600

    Update BitmapVisionImageGenerator.kt
    Comments

--- app/src/main/java/biz/aurafreelancersstudio/visionkit/core/visionimage/BitmapVisionImageGenerator.kt
+++ app/src/main/java/biz/aurafreelancersstudio/visionkit/core/visionimage/BitmapVisionImageGenerator.kt
@@ -3,7 +3,8 @@ package biz.aurafreelancersstudio.visionkit.core.visionimage
 import android.graphics.BitmapFactory
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 
-
+//Class uses method that converts the image to a format ML Kit uses to 
+//apply the computer vision methods onto it. This is similar to OpenCV
 class BitmapVisionImageGenerator(private val imagePath: String) : FirebaseVisionImageGenerator {
 
     override fun get(): FirebaseVisionImage = FirebaseVisionImage.fromBitmap(BitmapFactory.decodeFile(imagePath))
