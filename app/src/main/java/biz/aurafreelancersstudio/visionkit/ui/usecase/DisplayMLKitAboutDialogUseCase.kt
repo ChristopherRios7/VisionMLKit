4847d81e162661103d5ba2bd4995709f968d67c2
ChristopherRios7 <crios9694@gmail.com>
2018-11-05 11:49:10 -0600

    Update DisplayMLKitAboutDialogUseCase.kt

--- app/src/main/java/biz/aurafreelancersstudio/visionkit/ui/usecase/DisplayMLKitAboutDialogUseCase.kt
+++ app/src/main/java/biz/aurafreelancersstudio/visionkit/ui/usecase/DisplayMLKitAboutDialogUseCase.kt
@@ -3,7 +3,8 @@ package biz.aurafreelancersstudio.visionkit.ui.usecase
 import biz.aurafreelancersstudio.visionkit.core.model.MLKitApiOption
 import biz.aurafreelancersstudio.visionkit.core.wrapper.SharedPreferencesWrapper
 
-
+//if the the user has opened/used the app first time, the application will show
+//an about usecase dialog
 object DisplayMLKitAboutDialogUseCase {
 
     fun shouldShowAboutDialogFor(option: MLKitApiOption): Boolean {
