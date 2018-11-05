313e149a469a6d5fe100f1002a761ea909525a2a
ChristopherRios7 <crios9694@gmail.com>
2018-11-05 11:34:15 -0600

    Update MLKitAboutDialog.kt
    comments

--- app/src/main/java/biz/aurafreelancersstudio/visionkit/ui/dialog/MLKitAboutDialog.kt
+++ app/src/main/java/biz/aurafreelancersstudio/visionkit/ui/dialog/MLKitAboutDialog.kt
@@ -11,14 +11,16 @@ import biz.aurafreelancersstudio.visionkit.R
 class MLKitApiAboutDialog : DialogFragment() {
 
     private var listener: Listener? = null
-
+    
+    //attaches dialog to current context window
     override fun onAttach(context: Context?) {
         super.onAttach(context)
         if (context is Listener) {
             listener = context
         }
     }
-
+    
+    //creates the content of the dialog to tell firest
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val title = arguments?.getInt(KEY_TITLE) ?: 0
         val body = arguments?.getInt(KEY_BODY) ?: 0
@@ -34,7 +36,8 @@ class MLKitApiAboutDialog : DialogFragment() {
         const val KEY_TITLE = "key_title"
         const val KEY_BODY = "key_body"
     }
-
+   
+    //listens for user dismissal of the dialog
     interface Listener {
         fun onDismissed()
     }
